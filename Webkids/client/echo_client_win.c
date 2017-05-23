b0691791ab2b55d480dd07a0ab9a19f8dbed31f0
RedChiken <gombabo1217@gmail.com>
2017-05-23 15:04:28 +0900

    echo server and client in window

--- Webkids/client/echo_client_win.c
+++ Webkids/client/echo_client_win.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <WinSock2.h>
 #pragma warning(disable:4996)
-//inet_addr() 함수 사용 불가. InetPton을 사용해야 하나 사용 방법을 잘 몰라 임시로 지정.
+
+#define BUF_SIZE 1024
 void ErrorHandling(char *message);
 
 int main(int argc, char* argv[]) {
 	WSADATA wsaData;
 	SOCKET hSocket;
-	//socket 함수의 반호나값 저장을 위해 선언
+	char message[BUF_SIZE];
+	int strLen;
 	SOCKADDR_IN servAddr;
 
-	char message[30];
-	int strLen = 0;
-	int idx = 0, readLen = 0;
 	if (argc != 3) {
 		printf("Usage : %s <IP> <PORT>\n", argv[0]);
 		exit(1);
@@ -24,7 +24,6 @@ int main(int argc, char* argv[]) {
 	}
 
 	hSocket = socket(PF_INET, SOCK_STREAM, 0);
-	//socket 함수호출을 통해서 TCP 소켓을 생성
 	if (hSocket == INVALID_SOCKET) {
 		ErrorHandling("socket() error");
 	}
@@ -37,18 +36,24 @@ int main(int argc, char* argv[]) {
 	if (connect(hSocket, (SOCKET_ADDRESS*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR) {
 		ErrorHandling("connect() error!");
 	}
-
-	while (readLen = recv(hSocket, &message[idx++], 1, 0)) {
-		//recv 함수 호출을 통해 수신된 데이터를 1바이트씩 읽고 있다.
-		if (strLen == -1) {
-			ErrorHandling("read() error!");
-		}
-		strLen += readLen;
-		//41행에서 1바이트씩 읽고 있기 때문에 strLen의 값은 실제로 1씩 증가하고 있다.
+	else {
+		puts("Connected............");
 	}
-	
-	printf("Mdssage from server : %s\n", message);
-	printf("Function read call count: %d\n", strLen);
+
+	while (1) {
+		fputs("Input message(Q to quit): ", stdout);
+		fgets(message, BUF_SIZE, stdin);
+
+		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n")) {
+			break;
+		}
+		
+		send(hSocket, message, strlen(message), 0);
+		strLen = recv(hSocket, message, BUF_SIZE - 1, 0);
+		message[strLen] = 0;
+		printf("Message from server: %s", message);
+	}
+
 	closesocket(hSocket);
 	WSACleanup();
 	return 0;
--- Webkids/server/echo_server_win.c
+++ Webkids/server/echo_server_win.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <WinSock2.h>
+
+#define BUF_SIZE 1024
 void ErrorHandling(char *message);
 
 int main(int argc, char* argv[]) {
 	WSADATA wsaData;
 	SOCKET hServSock, hClntSock;
-	SOCKADDR_IN servAddr, clntAddr;
+	char message[BUF_SIZE];
+	int strLen, i;
 
+	SOCKADDR_IN servAddr, clntAddr;
 	int szClntAddr;
-	char message[] = "Hello World!";
 
 	if (argc != 2) {
 		printf(":Usage : %s <port>\main", argv[0]);
@@ -17,12 +21,10 @@ int main(int argc, char* argv[]) {
 	}
 
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		//소켓 라이브러리 초기화
 		ErrorHandling("WSAStartup() error");
 	}
 
 	hServSock = socket(PF_INET, SOCK_STREAM, 0);
-	//소켓 생성
 	if (hServSock == INVALID_SOCKET) {
 		ErrorHandling("socket() error");
 	}
@@ -33,28 +35,31 @@ int main(int argc, char* argv[]) {
 	servAddr.sin_port = htons(atoi(argv[1]));
 
 	if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR) {
-		//소켓에 IP주소와 PORT 번호를 할당
 		ErrorHandling("bind() error");
 	}
 
 	if (listen(hServSock, 5) == SOCKET_ERROR) {
-		// listen 함수 호출을 통해 24행에서 생성한 소켓을 서버 소켓으로 완성
 		ErrorHandling("listen() error");
 	}
 
 	szClntAddr = sizeof(clntAddr);
-	hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &szClntAddr);
-	//클라이언트의 연결 요청을 수락하기 위해서 accept 함수를 호출
-	if (hClntSock == INVALID_SOCKET) {
-		ErrorHandling("accept() error");
-	}
+	for (i = 0; i < 5; i++) {
+		hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &szClntAddr);
+		if (hClntSock == INVALID_SOCKET) {
+			ErrorHandling("accept() error");
+		}
+		else {
+			printf("Connected client %d \n", i + 1);
+		}
 
-	send(hClntSock, message, sizeof(message), 0);
-	// send 함수 호출을 통해 46행에서 연결된 클라이언트의 데이터를 전송
-	closesocket(hClntSock);
+		while ((strLen = recv(hClntSock, message, BUF_SIZE, 0)) != 0) {
+			send(hClntSock, message, sizeof(message), 0);
+		}
+
+		closesocket(hClntSock);
+	}
 	closesocket(hServSock);
 	WSACleanup();
-	//프로그램 종료 전 19행에서 초기화한 소켓 라이브러리를 해제
 	return 0;
 }
 
