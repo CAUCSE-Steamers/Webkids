24a877fa9f3e5a54b631b838aa2ccc2697762518
redchiken <gombabo1217@gmail.com>
2017-05-24 11:16:45 +0900

    echo client two

--- echo_client2.c
+++ echo_client2.c
@@ -13,7 +13,7 @@ int main(int argc, char* argv[])
 	int sock;
 	struct sockaddr_in serv_addr;
 	char message[BUF_SIZE];
-	int str_len = 0;
+	int str_len, recv_len, recv_cnt;
 
 	if(argc != 3)
 	{
@@ -49,8 +49,16 @@ int main(int argc, char* argv[])
 		{
 			break;
 		}
-		write(sock, message, strlen(message));
-		str_len = read(sock, message, BUF_SIZE - 1);
+		str_len = write(sock, message, strlen(message));
+
+		recv_len = 0;
+		while(recv_len < str_len){
+			recv_cnt = read(sock, &message[recv_len], BUF_SIZE - 1);
+			if(recv_cnt == -1){
+				error_handling("read() error!");
+			}
+			recv_len += recv_cnt;
+		}
 		message[str_len] = 0;
 		printf("Message from server : %s", message);
 	}
