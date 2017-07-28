fb051a8e41a5c4046c6911e0bc342a5f9b392fe6
redchiken <gombabo1217@gmail.com>
2017-07-28 13:48:00 +0900

    fix problem of thread4.c with mutex

--- mutex.c
+++ mutex.c
@@ -6,13 +6,16 @@
 
 void * thread_inc(void *arg);
 void * thread_des(void *arg);
+
 long long num = 0;
+pthread_mutex_t mutex;
 
 int main(int argc, char *argv[]){
     pthread_t thread_id[NUM_THREAD];
     int i;
 
-    printf("sizeof long long: %ld \n", sizeof(long long));        //long long의 크기 확인
+    pthread_mutex_init(&mutex, NULL);
+
     for(i = 0; i < NUM_THREAD; i++){
         if(i % 2){
             pthread_create(&(thread_id[i]), NULL, thread_inc, NULL);
@@ -21,17 +24,21 @@ int main(int argc, char *argv[]){
             pthread_create(&(thread_id[i]), NULL, thread_des, NULL);
         }
     }
+
     for(i = 0; i < NUM_THREAD; i++){
         pthread_join(thread_id[i], NULL);
     }
     printf("result: %lld \n", num);
+    pthread_mutex_destroy(&mutex);
     return 0;
 }
 
 void * thread_inc(void * arg){
     int i;
     for(i = 0; i < 50000000; i++){
+        pthread_mutex_lock(&mutex);
         num += 1;
+        pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
@@ -39,7 +46,9 @@ void * thread_inc(void * arg){
 void * thread_des(void * arg){
     int i;
     for(i = 0; i < 50000000; i++){
+        pthread_mutex_lock(&mutex);
         num -= 1;
+        pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
